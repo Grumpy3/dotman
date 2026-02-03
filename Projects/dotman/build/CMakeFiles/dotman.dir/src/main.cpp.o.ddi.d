04fa91e928b790232693ae713536a80f39eb64b8
Grumpy3 <piotrkoziol706@gmail.com>
2026-02-04 00:03:26 +0100

    Last final cleanup

--- Projects/dotman/build/CMakeFiles/dotman.dir/src/main.cpp.o.ddi.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/dotman.dir/src/main.cpp.o.ddi: \
- /home/piotr/Projects/dotman/src/main.cpp /usr/include/stdc-predef.h \
- /home/piotr/Projects/dotman/src/App.hpp
--- Projects/dotman/src/App.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-class App
-{
-public:
-    int run(int argc, char** argv);
-};
